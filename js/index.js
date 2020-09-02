1e413ad402b06e31336cfed4c9103ef84ed58ca8
unknown <ncuti60@gmail.com>
2020-09-02 15:26:03 +0200

    Add update article

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 const container = document.querySelector('.container')
+const commentForm = document.querySelector('.comment-form')
+const articleTitle = document.querySelector('#articleTitle')
+const articleContent = document.querySelector('#articleContent')
+const updateButton = document.querySelector('#sendComment')
+const closeBtn = document.querySelector('.closeBtn')
+
+closeBtn.addEventListener('click', () => {
+    commentForm.classList.toggle('active-comment')
+    container.classList.toggle('active-blur')
+})
 
 const renderPost = (doc) => {
     const post = document.createElement('div')
@@ -6,21 +16,28 @@ const renderPost = (doc) => {
 
     const title = document.createElement('h1')
     title.setAttribute('class', 'post-title')
-    title.textContent = doc.author
+    title.textContent = doc.title
 
     const content = document.createElement('p')
     content.setAttribute('class', 'post-content')
-    content.textContent = doc.content
+    if (doc.content.length > 150) {
+        content.textContent = doc.content.substr(0, 150) + '....'
+    }
 
     const deleteBtn = document.createElement('button')
     deleteBtn.setAttribute('class', 'post-delete')
     deleteBtn.textContent = "Delete"
+
+    const updateBtn = document.createElement('button')
+    updateBtn.setAttribute('class', 'post-update')
+    updateBtn.textContent = "update"
 
     post.setAttribute('data_id', doc._id)
 
     post.appendChild(title)
     post.appendChild(content)
     post.appendChild(deleteBtn)
+    post.appendChild(updateBtn)
 
     container.appendChild(post)
 
@@ -28,19 +45,64 @@ const renderPost = (doc) => {
         let varConfirm = confirm("Are you sure you want to delete🙄")
         if (varConfirm == true) {
             let id = e.target.parentElement.getAttribute('data_id')
-            console.log(id)
-            fetch(`http://127.0.0.1:5020/api/v1/blogs/${id}`, {
+            fetch(`https://morning-thicket-92126.herokuapp.com/api/v1/blogs/${id}`, {
                 method: 'DELETE',
                 headers: {
                     "Content-Type": "application/json; charset=utf-8"
                 }
             }).then(res => res.json())
-                .then(result => console.log(result))
             return true
         } else {
             return false
         }
 
+    })
+
+    updateBtn.addEventListener('click', (e) => {
+
+        let id = e.target.parentElement.getAttribute('data_id')
+        
+        //open update form
+        commentForm.classList.toggle('active-comment')
+        container.classList.toggle('active-blur')
+
+        //Get title and content
+        fetch(`https://morning-thicket-92126.herokuapp.com/api/v1/blogs/${id}`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            }
+        }).then(res => res.json())
+            .then(res => {
+                articleTitle.value = res.data.article.title
+                articleContent.value = res.data.article.content
+            })
+
+        //update
+        updateButton.addEventListener('click', (e) => {
+            var refVar = confirm('Are you sure to update this article?')
+            if (refVar == true) {
+                fetch(`https://morning-thicket-92126.herokuapp.com/api/v1/blogs/${id}`, {
+                    method: 'PATCH',
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8",
+                        "Authorization": `Bearer ${localStorage.token}`
+                    },
+                    body: JSON.stringify({
+                        title: articleTitle.value,
+                        content: articleContent.value
+                    })
+                }).then(res => res.json())
+                    .then(res => {
+                        articleTitle.value = ''
+                        articleContent.value = ''
+                        location.reload()
+                    })
+            } else {
+                return false
+            }
+
+        })
     })
 
 }
@@ -62,14 +124,12 @@ const posts = [
     }
 ]
 
-console.log(posts)
 
 // posts.forEach(el => renderPost(el))
 
-fetch('http://127.0.0.1:5020/api/v1/blogs', {
+fetch('https://morning-thicket-92126.herokuapp.com/api/v1/blogs', {
     method: 'GET'
-}).then(res=>res.json())
-.then(dt=>{
-    console.log(dt)
-    dt.data.articles.forEach(article => renderPost(article))
-})
+}).then(res => res.json())
+    .then(dt => {
+        dt.data.articles.forEach(article => renderPost(article))
+    })
